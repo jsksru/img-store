b0607390b05178890dccff6a79503e0ab9923df9
jsksru <mail@jsks.ru>
2021-02-17 19:28:59 +0500

    config file

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  port: 3000,
+  prefix: '/api/v1',
+  secret: '6021a76a99ca4d1d5a467de712ccf6875b91da1c2c5aa98b714012cbacc95a8c15d9feec2283b39b6f826a16c042e3768f5cc9bf2a790386028b1e103e1c2d6e',
+  uploadDir: 'uploads',
+  allowTypes: ['image/jpeg', 'image/png'],
+  maxSize: 5000000,
+  dimensions: {
+    thumb: {
+      fileName: 'thumb',
+      maxWidth: 50,
+      maxHeight: 50,
+    },
+    small: {
+      fileName: 'small',
+      maxWidth: 150,
+      maxHeight: 150,
+    },
+    medium: {
+      fileName: 'medium',
+      maxWidth: 500,
+      maxHeight: 500,
+    }
+  }
+};
