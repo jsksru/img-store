d409dbfae22035a032d769565085edc52a30ad1f
jsksru <mail@jsks.ru>
2021-02-19 18:31:57 +0500

    split code add renamer

--- workers/imageProcessor.worker.js
+++ workers/imageProcessor.worker.js
@@ -1,25 +1,7 @@
 const config = require('../config');
 const db = require('../db');
-const sizeOf = require('image-size');
 const {isMainThread, parentPort} = require('worker_threads');
-const Jimp = require('jimp');
-
-const imageResize = async (imageDir, extension, maxWidth, maxHeight, newName) => {
-  try {
-    const file = await Jimp.read(`./${imageDir}/original.${extension}`);
-    file.scaleToFit(maxWidth, maxHeight);
-    await file.writeAsync(`./${imageDir}/${newName}.${extension}`);
-    const dimensions = await sizeOf(`./${imageDir}/${newName}.${extension}`);
-    return {
-      size: newName,
-      src: `${imageDir}/${newName}.${extension}`,
-      width: dimensions.width,
-      height: dimensions.height,
-    };
-  } catch(err) {
-    throw new Error(err.message);
-  }
-};
+const imageTransform = require('../utils/imageTransform');
 
 if (!isMainThread) {
   parentPort.on('message', async ({imageDir, extension, imageId}) => {
@@ -27,11 +9,12 @@ if (!isMainThread) {
       const sizesCollector = [];
 
       for (size of config.dimensions) {
-        const result = await imageResize(imageDir, extension, size.maxWidth, size.maxHeight, size.fileName);
+        const result = await imageTransform.imageResize(imageDir, extension, size.maxWidth, size.maxHeight, size.fileName);
         sizesCollector.push(result);
       }
 
-      await db.editById(imageId, { complete: true, images: sizesCollector });
+      const newOriginalPath = await imageTransform.renameOriginal(imageDir, extension);
+      await db.editById(imageId, { original: newOriginalPath, complete: true, images: sizesCollector });
 
       parentPort.postMessage(`Processing image with id=${imageId} complete!`);
     } catch(err) {
