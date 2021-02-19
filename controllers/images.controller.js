64d1c39f6d24c2c7903c993b6ee30b2a9ca8927e
jsksru <mail@jsks.ru>
2021-02-19 18:31:17 +0500

    images controller

---
+++ controllers/images.controller.js
@@ -0,0 +1,15 @@
+const db = require('../db');
+
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) throw new Error('Incorrect or empty ID');
+    const image = await db.getById(id);
+    if (!image) return res.status(404).json({ error: true, message: 'Image Not Found!' });
+    return res.status(200).json(image);
+  } catch (err) {
+    return next(new Error(err.message));
+  }
+};
+
+module.exports.getById = getById;
