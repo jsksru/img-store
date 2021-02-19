fd0a01ffaa3065188efc0f2fb7243127fbca5c6e
jsksru <mail@jsks.ru>
2021-02-19 18:56:29 +0500

    add settimeout for queue

--- workers/imageProcessor.worker.js
+++ workers/imageProcessor.worker.js
@@ -4,19 +4,21 @@ const {isMainThread, parentPort} = require('worker_threads');
 const imageTransform = require('../utils/imageTransform');
 
 if (!isMainThread) {
-  parentPort.on('message', async ({imageDir, extension, imageId}) => {
+  parentPort.on('message', ({imageDir, extension, imageId}) => {
     try {
-      const sizesCollector = [];
+      setTimeout(async () => {
+        const sizesCollector = [];
 
-      for (size of config.dimensions) {
-        const result = await imageTransform.imageResize(imageDir, extension, size.maxWidth, size.maxHeight, size.fileName);
-        sizesCollector.push(result);
-      }
-
-      const newOriginalPath = await imageTransform.renameOriginal(imageDir, extension);
-      await db.editById(imageId, { original: newOriginalPath, complete: true, images: sizesCollector });
-
-      parentPort.postMessage(`Processing image with id=${imageId} complete!`);
+        for (size of config.dimensions) {
+          const result = await imageTransform.imageResize(imageDir, extension, size.maxWidth, size.maxHeight, size.fileName);
+          sizesCollector.push(result);
+        }
+  
+        const newOriginalPath = await imageTransform.renameOriginal(imageDir, extension);
+        await db.editById(imageId, { original: newOriginalPath, complete: true, images: sizesCollector });
+  
+        parentPort.postMessage(`Processing image with id=${imageId} complete!`);
+      }, 0);
     } catch(err) {
       throw new Error(err.message);
     }
