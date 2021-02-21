b93d1246b8a1c048508836960e2b1b5f4cfa47aa
jsksru <mail@jsks.ru>
2021-02-21 14:38:13 +0500

    modify get image method

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -6,6 +6,7 @@ const getById = async (req, res, next) => {
     if (!id) throw new Error('Incorrect or empty ID');
     const image = await db.getById(id);
     if (!image) return res.status(404).json({ error: true, message: 'Image Not Found!' });
+    if (image.complete && !req.isAuth) image.original = null;
     return res.status(200).json(image);
   } catch (err) {
     return next(new Error(err.message));
